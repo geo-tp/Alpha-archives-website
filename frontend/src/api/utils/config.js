c99ed9e78f0ca6e343f7a4989d5076ec339af9c1
geo <tp53.geo@gmail.com>
2022-11-16 01:37:53 +0100

    change manifest and update docs

--- frontend/src/api/utils/config.js
+++ frontend/src/api/utils/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "https://archive.thealphaproject.eu/";
-// export const API_URL = "http://localhost:8001/";
+// export const API_URL = "https://archive.thealphaproject.eu/";
+export const API_URL = "http://localhost:8001/";
 
 export const BASIC_HEADER = new Headers({
   "Content-Type": "application/json",

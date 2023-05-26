274e70e771c0ec502da673ccee871777113bc215
geo <tp53.geo@gmail.com>
2023-05-26 20:00:12 +0200

    change CharField for image_raw to ImageField to be able to use django default media behaviour

--- backend/file/models.py
+++ backend/file/models.py
@@ -6,8 +6,7 @@ class File(models.Model):
     filename = models.CharField(max_length=1024)
     is_folder = models.BooleanField(default=False)
 
-    # we use a CharField here to be able to build database from archives without deal with File
-    image_raw = models.CharField(max_length=1024, blank=True, null=True)
+    image_raw = models.ImageField(blank=True, null=True, max_length=4096)
 
     image_thumbnail = models.ImageField(blank=True, null=True)
     image_hash = models.CharField(max_length=256, blank=True, null=True)
